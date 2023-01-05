107e3cdccbbdc5de471f25d98eaa9904f2db8507
Andrej Tasevski Mestrovic <akitasevski112@gmail.com>
2023-01-05 21:34:57 +0100

    added store pahge

---
+++ LegoProdavnica/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LegoProdavnica.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult IndexProduct()
+        {
+            return View();
+        }
+    }
+}
